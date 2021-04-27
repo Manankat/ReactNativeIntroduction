15d0b3fe5680a4008fc2a5548ca2ca8eba7daa9b
Victor <victor.laplagne@epitech.eu>
2021-04-27 14:20:15 -0400

    username api

--- App.js
+++ App.js
@@ -1,61 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, FlatList } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+  ScrollView
+} from 'react-native';
 
-const App = () => {
-  const [search, setSearch] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
+const screenWidth = Dimensions.get('window').width;
+export default class App extends Component {
+  state = {
+    username: '',
+    repos: [],
+  }
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
+  handleChange = (evt) => {
+    this.setState({
+      username: evt.nativeEvent.text
+    });
+  }
+
+  getUserRepos = (username) => {
+    username = username.toLowerCase().trim();
+    const url = `https://api.github.com/users/${username}/repos`;
+    return fetch(url).then((res) => res.json());
+  }
+
+  handleSubmit = () => {
+    this.getUserRepos(this.state.username)
+      .then((res) => {
+        this.setState({repos: res});
       });
-  }, []);
+  }
 
-  const searchFilterFunction = (text) => {
-    if (text) {
-      const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
-    }
-  };
+  renderRepos = () => {
+    return (
+      <ScrollView>
+        {
+          this.state.repos.map((repo, i) => {
+            return (
+              <View key={i}>
+                <Text>{i}, {JSON.stringify(repo.full_name)}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+    )
+  }
 
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.header}>
-					<Text style={styles.subTitle}>Workshop</Text>
-				</View>
-      </SafeAreaView>
-      <View >
-          <SearchBar
-            round
-            searchIcon={{ size: 24 }}
-            onChangeText={(text) => searchFilterFunction(text)}
-            onClear={(text) => searchFilterFunction('')}
-            placeholder="Type Here..."
-            value={search}
-          />
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>GitHub Username</Text>
+        <TextInput
+          placeholder="Enter your github username"
+          style={styles.input}
+          onChange={this.handleChange}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.8}
+          onPress={this.handleSubmit}
+          >
+          <Text style={styles.buttonText}>VIEW</Text>
+        </TouchableOpacity>
+        { this.renderRepos() }
       </View>
-    </>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   
@@ -75,5 +92,3 @@ const styles = StyleSheet.create({
 		fontSize: 40
 	},
 });
-
-export default App;
