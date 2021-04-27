8ccd4de2587d388c34bbc98be2599ff65aa33adf
esupper <edouard.supper@epitech.eu>
2021-04-27 18:02:34 +0200

    [+] La Navigacion

--- App.js
+++ App.js
@@ -1,79 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, FlatList } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React from "react";
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, ThemeProvider } from 'react-native-elements';
 
-const App = () => {
-  const [search, setSearch] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
+const Stack = createStackNavigator();
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const searchFilterFunction = (text) => {
-    if (text) {
-      const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
-    }
-  };
-
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.header}>
-					<Text style={styles.subTitle}>Workshop</Text>
-				</View>
-      </SafeAreaView>
-      <View >
-          <SearchBar
-            round
-            searchIcon={{ size: 24 }}
-            onChangeText={(text) => searchFilterFunction(text)}
-            onClear={(text) => searchFilterFunction('')}
-            placeholder="Type Here..."
-            value={search}
-          />
-      </View>
-    </>
-  );
+const HomeScreen = ({ navigation }) => {
+    return (
+        <>
+            <Button
+                title="Repositories"
+                style={styles.button}
+                onPress={() =>
+                    navigation.navigate('Repositories')
+                }
+            />
+            <Button
+                title="Users"
+                style={styles.button}
+                onPress={() =>
+                    navigation.navigate('Users')
+                }
+            />
+        </>
+    )
 };
 
+const UsersScreen = ({ navigation, route }) => {
+    return (
+        <Text>
+        </Text>
+    )
+};
+
+const UserScreen = ({ navigation, route }) => {
+    return (
+        <Text>
+        </Text>
+    )
+};
+
+const RepositoriesScreen = ({ navigation, route }) => {
+    return (
+        <Text>
+        </Text>
+    )
+};
+
+const RepositoryScreen = ({ navigation, route }) => {
+    return (
+        <Text>
+        </Text>
+    )
+};
+
+
+const App = () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ title: 'Welcome' }}
+            />
+
+            <Stack.Screen
+                name="Users"
+                component={UsersScreen}
+                options={{ title: 'Users' }}
+            />
+            <Stack.Screen
+                name="User"
+                component={UserScreen}
+                options={{ title: 'User' }}
+            />
+            <Stack.Screen
+                name="Repositories"
+                component={RepositoriesScreen}
+                options={{ title: 'Repositories' }}
+            />
+            <Stack.Screen
+                name="Repository"
+                component={RepositoryScreen}
+                options={{ title: 'Repository' }}
+            />
+
+        </Stack.Navigator>
+    </NavigationContainer>
+
+);
+
 const styles = StyleSheet.create({
-  
-  header:
-	{
-		height: '8%',
-		alignItems: 'center',
-		backgroundColor: 'black',
-		justifyContent: 'space-between',
-	},
-  itemStyle: {
-    padding: 10,
-  },
-  subTitle: {
-		color: 'white',
-		textAlign: 'center',
-		fontSize: 40
-	},
+    header:
+    {
+        height: '8%',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        justifyContent: 'space-between',
+    },
+    itemStyle: {
+        padding: 10,
+    },
+    subTitle: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 40
+    },
+    button: {
+        padding: 10
+    }
 });
 
 export default App;
