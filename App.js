45e98af7c65463c75aa7a14502b8a758857c7233
Victor <victor.laplagne@epitech.eu>
2021-04-27 13:12:08 -0400

    design_github

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, View, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
-export default function App() {
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    if (text) {
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.header}>
+					<Text style={styles.subTitle}>Workshop</Text>
+				</View>
+      </SafeAreaView>
+      <View >
+          <SearchBar
+            round
+            searchIcon={{ size: 24 }}
+            onChangeText={(text) => searchFilterFunction(text)}
+            onClear={(text) => searchFilterFunction('')}
+            placeholder="Type Here..."
+            value={search}
+          />
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+  header:
+	{
+		height: '8%',
+		alignItems: 'center',
+		backgroundColor: 'black',
+		justifyContent: 'space-between',
+	},
+  itemStyle: {
+    padding: 10,
   },
+  subTitle: {
+		color: 'white',
+		textAlign: 'center',
+		fontSize: 40
+	},
 });
+
+export default App;
---
+++ src/Theme/color.js
@@ -0,0 +1,11 @@
+export const colors = {
+	black: 'black',
+	white: '#FAFAF7',
+	darkGrey : '#323230',
+	mediumGrey: '#626567',
+	lightGrey: '#979A9A',
+	darkPurple: '#5B2C6F',
+	mediumPurple: '#6C3483',
+	lightPurple: '#bba5e1',
+	yellow: '#f8e8a2'
+}
