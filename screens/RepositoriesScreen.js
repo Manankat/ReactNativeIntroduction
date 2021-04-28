4dba1a77837da81691c595947020b18f2e1f43c0
Victor <victor.laplagne@epitech.eu>
2021-04-28 14:24:37 -0400

    add repositoriesscreen & index.js

--- screens/RepositoriesScreen.js
+++ screens/RepositoriesScreen.js
@@ -4,9 +4,57 @@ import { StyleSheet, Text, View, AppRegistry } from "react-native";
 import { Button, ThemeProvider } from 'react-native-elements';
 
 export const RepositoriesScreen = ({ navigation, route }) => {
+    const [username, setName] = useState('');
+    const [login, setLogin] = useState('');
+    const [error, setError] = useState('');
+    const [uInput, setUInput] = useState('');
+
+    const setData = ({
+        username
+    }) => {
+      setName(username);
+    }
+
+    const userResearch = (e) => {
+        setUInput(e);
+        console.log(e)
+    }
+
+    const userSubmit = () => {
+        username = username.toLowerCase().trim();
+        fetch(`https://api.github.com/users/${username}/repos`)
+          .then(res => res.json())
+          .then(data => {
+            if (data.message) {
+                  setError(data.message)
+            } else {
+                  setData(data);
+                  setError(null);
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
     return (
-        <Text>
-        </Text>
+        <>
+        <TextInput
+            style={styles.userInput}
+            placeholder='Users'
+            onChangeText={(uInput) =>
+                userResearch(uInput)
+            }
+            defaultValue={uInput}
+        />
+        <Button
+            title='Search Users'
+            style={styles.button}
+            onPress={() =>
+                userSubmit()
+            }
+        />
+        <Text>{username} {login}</Text>
+    </>
     )
 };
 
