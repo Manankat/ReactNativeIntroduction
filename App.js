7b701693915519e21b82d825a3c35771d2ff00d0
Victor <victor.laplagne@epitech.eu>
2021-04-28 14:13:43 -0400

    place App Navigator

--- App.js
+++ App.js
@@ -1,87 +1,45 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Dimensions,
-  TouchableOpacity,
-  Alert,
-  ScrollView
-} from 'react-native';
+import React from "react";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, UsersScreen, UserScreen, RepositoriesScreen, RepositoryScreen } from './screens'
 
-export default class getRepositoriesbyUsername extends Component {
-  state = {
-    repository: '',
-    repos: [],
-  }
+const Stack = createStackNavigator();
 
-  handleChange = (evt) => {
-    this.setState({
-      repository: evt.nativeEvent.text
-    });
-  }
 
-  getRepository = (repository) => {
-    repository = repository.toLowerCase().trim();
-    const url = `https://api.github.com/repos/${repository}`;
-    return fetch(url).then((res) => res.json());
-  }
+const App = () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ title: 'Welcome' }}
+            />
+            <Stack.Screen
+                name="Users"
+                component={UsersScreen}
+                options={{ title: 'Users' }}
+            />
+            <Stack.Screen
+                name="User"
+                component={UserScreen}
+                options={{ title: 'User' }}
+            />
+            <Stack.Screen
+                name="Repositories"
+                component={RepositoriesScreen}
+                options={{ title: 'Repositories' }}
+            />
+            <Stack.Screen
+                name="Repository"
+                component={RepositoryScreen}
+                options={{ title: 'Repository' }}
+            />
 
-  handleSubmit = () => {
-    this.getRepository(this.state.repository)
-      .then((res) => {
-        this.setState({repos: res});
-      });
-  }
+        </Stack.Navigator>
+    </NavigationContainer>
 
-  renderRepos = () => {
-    return (
-      <ScrollView>
-              <View>
-                <Text>{JSON.stringify(this.state.repos.full_name)}</Text>
-              </View>
-      </ScrollView>
-    )
-  }
+);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>GitHub Username</Text>
-        <TextInput
-          placeholder="Enter your github username/repository"
-          style={styles.input}
-          onChange={this.handleChange}
-        />
-        <TouchableOpacity
-          style={styles.button}
-          activeOpacity={0.8}
-          onPress={this.handleSubmit}
-          >
-          <Text style={styles.buttonText}>VIEW</Text>
-        </TouchableOpacity>
-        { this.renderRepos() }
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  
-  header:
-	{
-		height: '8%',
-		alignItems: 'center',
-		backgroundColor: 'black',
-		justifyContent: 'space-between',
-	},
-  itemStyle: {
-    padding: 10,
-  },
-  subTitle: {
-		color: 'white',
-		textAlign: 'center',
-		fontSize: 40
-	},
-});
+export default App;
